2a49189ee8398a61791a12bde8d33cb9a65a3bd2
Lane Walker <lane.walker@tccd.edu>
2025-09-15 07:35:25 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,35 @@
+
+
+// Remove something to make it work
+fn main() {
+    let x: i32 = 5;
+    let mut y: u32 = 5;
+
+    y = x;
+    
+    let z = 10; // Type of z ? 
+
+    println!("Success!");
+}
+
+
+// Fill the blank
+fn main() {
+    let v: u16 = 38_u8 as __;
+
+    println!("Success!");
+}
+
+
+// Fill the blanks to make it work
+fn main() {
+    assert_eq!(i8::MAX, __); 
+    assert_eq!(u8::MAX, __); 
+
+    println!("Success!");
+}
+
+
 /// Return `12` if `n` is even,
 /// `13` if `n` is divisible by `3`,
 /// `17` otherwise.
